5bf5b4bc2475a131f677a7f133fd15ece8eb550c
Gahtao <gahtao.amachigato@outlook.com>
2017-07-02 20:51:43 +0200

    config update client

---
+++ client/config/Reika/Cave_Definitions/global.lua
@@ -0,0 +1,17 @@
+{
+	type = "global"
+	cave_size = 1.0
+	density = 1.0
+	dungeons = 1.0
+	gen_strongholds = true
+	large_cave_chance = 25.0
+	large_cave_size = 1.0
+	large_cave_variance = 6.0
+	lava_caves = true
+	max_y = 256
+	min_y = 0
+	mineshafts = 1.0
+	ravines = 1.0
+	vscale = 1.0
+	water_caves = false
+}
